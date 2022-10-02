6c24a6c48f92374a21ed959de2fce31b2583ff6a
Andrea Piseri <andrea.piseri@gmail.com>
2022-10-02 17:19:24 +0200

    extract `lib` as a submodule

--- lib/error/module.c
+++
@@ -1,9 +0,0 @@
-#include "../builtin.h"
-#include "module.h"
-
-cstring runtime_error_strings[Num_Errors] = {
-	[None]              = "None",
-	[Empty_Parse]       = "Empty_Parse",
-	[Invalid_Exponent]  = "Invalid_Exponent",
-	[Invalid_Parse]     = "Invalid_Parse",
-};
